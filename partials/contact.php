44e7dad9aeb7994ded7d5fad93dfc403b6a1cc00
jerzy-olender <olender.jerzy@gmail.com>
2025-09-08 11:14:16 +0200

    added partials and mixins contact section part

--- partials/contact.php
+++ partials/contact.php
@@ -1,9 +1,31 @@
 <section class="contact">
-  <h2>Porozmawiajmy</h2>
-  <form action="#" method="post">
-    <input type="text" name="name" placeholder="Imię i nazwisko" required>
-    <input type="email" name="email" placeholder="Adres e-mail" required>
-    <textarea name="message" rows="5" placeholder="Wiadomość"></textarea>
-    <button type="submit">Wyślij</button>
-  </form>
+  <div class="container">
+    <h2 class="contact-heading">Porozmawiajmy</h2>
+    <div class="form-wrapper">
+      <h3 class="contact-subheading">
+        Zostaw nam wiadomość
+      </h3>
+      <form action="#" method="post">
+        <div class="inputs-wrapper">
+          <div class="field-wrapper">
+            <label class="field-label" for="name">Imię i nazwisko</label>
+            <input type="text" id="name" name="name" placeholder="Imię i nazwisko" required>
+          </div>
+          <div class="field-wrapper">
+            <label class="field-label" for="email">Adres e-mail</label>
+            <input type="email" id="email" name="email" placeholder="Adres e-mail" required>
+          </div>
+          <div class="field-wrapper">
+            <label class="field-label" for="subject">Temat</label>
+            <input type="text" id="subject" name="subject" placeholder="Temat" required>
+          </div>
+        </div>
+        <div class="field-wrapper">
+          <label class="field-label" for="message">Wiadomość</label>
+          <textarea id="message" name="message" placeholder="Wiadomość"></textarea>
+        </div>
+        <button type="submit">Wyślij</button>
+      </form>
+    </div>
+  </div>
 </section>
