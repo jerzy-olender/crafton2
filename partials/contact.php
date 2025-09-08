7ee15e1bd999271e9baf1cf14ca68b427edc5537
jerzy-olender <olender.jerzy@gmail.com>
2025-09-09 01:11:56 +0200

    added footer

--- partials/contact.php
+++ partials/contact.php
@@ -28,6 +28,7 @@
           <input type="checkbox" id="acceptance" class="chk" name="acceptance">
           <label for="acceptance" class="chk-label">
             Wyrażam zgodę na przetwarzanie moich danych osobowych w postaci imienia, nazwiska, adresu e-mail i nr tel. (jeżeli został podany), podanych w powyższym formularzu, zgodnie z przepisami rozporządzenia Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (ogólne rozporządzenie o ochronie danych), Dz. Urz. UE z 4.5.2016 r. L 119, str. 1), w celu udzielenia odpowiedzi na złożone zapytanie. Żądanie usunięcia danych proszę kierować na adres biuro@realestate.com
+            <span>Informujemy, że: 1. Administratorem Pani/Pana danych osobowych jest RealEstate sp. z o.o. z siedzibą w Poznaniu przy ul. Długiej 5 lok. 25, 01-200 Poznań (KRS nr 0001000000) (dalej „Administrator”) e-mail: biuro@realestate.com</span>
           </label>
         </div>
         <button class="send" type="submit">Wyślij wiadomość</button>
--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,7 @@
-<footer>
-  <p>&copy; <?php echo date("Y"); ?> Crafton – Wszystkie prawa zastrzeżone</p>
+<footer class="main-footer">
+  <div class="container">
+    <p class="copyright">&copy; <?php echo date("Y"); ?> RealEstate Sp. z o.o. – Wszelkie prawa zastrzeżone</p>
+    <p class="copyright">Projekt i realizacja: <a href="#">Crafton</a></p>
+    <p class="copyright">Materiały zawarte na stronie WWW mają charakter poglądowy i nie mogą być traktowane jako ostateczne projekty realizacyjne. Deweloper zastrzega<br> sobie prawo zmian. Niniejsza informacja nie stanowi oferty w rozumieniu przepisów Kodeksu Cywilnego i ma wyłącznie charakter informacyjny.</p>
+  </div>
 </footer>
