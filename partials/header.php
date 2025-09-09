d886674ca7386f84c93ca02a8aeb2e25d3d52197
jerzy-olender <olender.jerzy@gmail.com>
2025-09-09 08:43:28 +0200

    added menu continue

--- partials/header.php
+++ partials/header.php
@@ -7,9 +7,11 @@
       <img class="img-fluid hamburger" src="images/hamburger.svg" alt="Logo">
       <nav>
         <ul class="main-header-list">
-          <li class="main-header-list-item"><a class="main-header-list-item-link" href="#">Strona główna</a></li>
+          <li class="main-header-list-item"><a class="main-header-list-item-link" href="#">Home</a></li>
+          <li class="main-header-list-item"><a class="main-header-list-item-link" href="#">O nas</a></li>
           <li class="main-header-list-item"><a class="main-header-list-item-link" href="#">Nasze inwestycje</a></li>
           <li class="main-header-list-item"><a class="main-header-list-item-link" href="#">Poradnik</a></li>
+           <li class="main-header-list-item"><a class="main-header-list-item-link" href="#">Wynajmij</a></li>
         </ul>
       </nav>
       <a href="" class="additional-link">Kontakt <img class="button-arrow" src="images/button-arrow.svg" alt="button arrow"></a>
